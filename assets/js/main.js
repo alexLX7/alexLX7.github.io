981e4d7cde79cb0ce995802b327e0a8df764ea76
alexLX7 <dat1chilldevm8@yandex.ru>
2020-09-30 22:42:10 +0300

    Added a slideshow

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,22 +4,42 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-var slideIndex = 1;
-showDivs(slideIndex);
+// var slideIndex = 1;
+// showDivs(slideIndex);
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
+// function plusDivs(n) {
+//   showDivs(slideIndex += n);
+// }
 
-function showDivs(n) {
+// function showDivs(n) {
+//   var i;
+//   var x = document.getElementsByClassName("manual slides");
+//   if (n > x.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = x.length}
+//   for (i = 0; i < x.length; i++) {
+// 	x[i].style.display = "none";  
+//   }
+//   x[slideIndex-1].style.display = "block";  
+// }
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
   var i;
-  var x = document.getElementsByClassName("manual slides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-	x[i].style.display = "none";  
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
   }
-  x[slideIndex-1].style.display = "block";  
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" activeslide", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " activeslide";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
 
 (function($) {
