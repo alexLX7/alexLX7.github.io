6cb74e8372cb6177e57eb0db4e9bc84c93cbae96
alexLX7 <dat1chilldevm8@yandex.ru>
2020-10-04 23:11:27 +0300

    Updated title of slideshow, deleted redundant code

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,30 +4,12 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-// var slideIndex = 1;
-// showDivs(slideIndex);
-
-// function plusDivs(n) {
-//   showDivs(slideIndex += n);
-// }
-
-// function showDivs(n) {
-//   var i;
-//   var x = document.getElementsByClassName("manual slides");
-//   if (n > x.length) {slideIndex = 1}
-//   if (n < 1) {slideIndex = x.length}
-//   for (i = 0; i < x.length; i++) {
-// 	x[i].style.display = "none";  
-//   }
-//   x[slideIndex-1].style.display = "block";  
-// }
-
 var slideIndex = 0;
 showSlides();
 
 function showSlides() {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
+  var slides = document.getElementsByClassName("slideshow");
   var dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
